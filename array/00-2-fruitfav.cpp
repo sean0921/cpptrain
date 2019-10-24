db7a3f65729be7279ea4a092b6b9ff097cab38e6
sean0921 <sean.li.shin.ho@gmail.com>
2019-10-24 20:35:59 +0800

    add practice 0

---
+++ array/00-2-fruitfav.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    string Fruit[]={"banana","apple","guava"};
+    cout << "Fruit 1: " << Fruit[0] << "\n";
+    cout << "Fruit 2: " << Fruit[1] << "\n";
+    cout << "Fruit 3: " << Fruit[2] << "\n";
+    return 0;
+}
