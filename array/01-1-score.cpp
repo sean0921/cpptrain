1d63a28cfb2596d93f5ecff7c56239bbe9ec498b
sean0921 <sean.li.shin.ho@gmail.com>
2019-10-25 13:52:02 +0800

    add 01-1 program

---
+++ array/01-1-score.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    const int n=5;
+    int score[n];
+    double sum=0, ave=0;
+    for(int i=0; i<n; i=i+1) {
+        cout << "請輸入第 " << i+1 << " 位選手的成績：";
+        cin >> score[i];
+    }
+
+    for(int i=0; i<n; i=i+1) {
+        sum += score[i];
+    }
+
+    ave = sum/n;
+    cout << "全隊總成績為：" << sum << " 分，平均為" << ave << " 分 \n";
+    return 0;
+}
